8ce74e581c0c988488f82b2b151eb65966704c29
Raficknew <rafi20302@gmail.com>
2024-07-18 18:58:18 +0200

    race model

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import LoginButton from "./components/atoms/LoginButtons/Loginbutton";
 import Navbar from "./components/organizms/Navbar/Navbar";
 import Link from "next/link";
+import prisma from "@/lib/db/prisma";
 import { buttonVariants } from "@/components/ui/button";
 import { Flag } from "lucide-react";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
+  const races = await prisma.race.findMany({
+    orderBy: { id: "desc" },
+    include: {
+      user: true,
+    },
+  });
   return (
     <div>
       <Navbar />
@@ -25,7 +31,11 @@ export default async function Home() {
           </Link>
         )}
         <div className="flex flex-col self-stretch items-center gap-5">
-          <div className="flex bg-gray-400 h-[93px] self-stretch"></div>
+          <div className=" bg-gray-400 h-[93px] self-stretch">
+            <p className="text-black h-3 w-3">{races[0].name}</p>
+            <p className="text-black h-3 w-3">{races[0].circuit}</p>
+            <p>{races[0].user?.name}</p>
+          </div>
           <div className="flex bg-gray-400 h-[93px] self-stretch"></div>
           <div className="flex bg-gray-400 h-[93px] self-stretch"></div>
           <div className="flex bg-gray-400 h-[93px] self-stretch"></div>
